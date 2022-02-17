ec8d0921fbef11ebb42bf5752d66601022823368
faozibouybaouenegadrouz <fbg171@gmail.com>
2022-02-17 19:10:21 +0100

    Toast Correct done

--- app/src/main/java/com/example/ddm1_geoquiz/MainActivity.java
+++ app/src/main/java/com/example/ddm1_geoquiz/MainActivity.java
@@ -17,13 +17,28 @@ import com.example.ddm1_geoquiz.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
