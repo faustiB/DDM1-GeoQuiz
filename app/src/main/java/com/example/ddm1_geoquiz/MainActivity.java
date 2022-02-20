c7d7c6b52256e1b0abf8a8f868e36bdebcf0aa04
faozibouybaouenegadrouz <fbg171@gmail.com>
2022-02-20 17:51:41 +0100

    implemented multiple questions, and checks of questions

--- app/src/main/java/com/example/ddm1_geoquiz/MainActivity.java
+++ app/src/main/java/com/example/ddm1_geoquiz/MainActivity.java
@@ -18,12 +18,23 @@ import com.example.ddm1_geoquiz.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private TextView tvQuestion;
+
+    private HashMap<String,Boolean> questions = new HashMap<>();
+    private int first_time = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,23 +44,69 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
 
+        tvQuestion = findViewById(R.id.tv_question);
+
+
+        if (first_time == 1){
+            questions.put("Barcelona is the capital of Spain",false);
+            questions.put("Madrid is the capital of Spain",true);
+            questions.put("Casablanca is the capital of Morrocco",false);
+            questions.put("Rabat is the capital of Morocco",true);
+            questions.put("Nantes is the capital of France",false);
+            questions.put("Paris is the capital of France",true);
+
+            String randomQuestion = setNextQuestion();
+            tvQuestion.setText(randomQuestion);
+            first_time++;
+        }
+
+
+
+
+
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+
+                if (questions.get(tvQuestion.getText())){
+                    Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this,R.string.wrong_toast,Toast.LENGTH_SHORT).show();
+                }
+
+                String randomQuestion = setNextQuestion();
+                tvQuestion.setText(randomQuestion);
+
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,R.string.wrong_toast,Toast.LENGTH_SHORT).show();
+
+                if (!questions.get(tvQuestion.getText())){
+                    Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this,R.string.wrong_toast,Toast.LENGTH_SHORT).show();
+                }
+
+                String randomQuestion = setNextQuestion();
+                tvQuestion.setText(randomQuestion);
             }
         });
 
     }
 
+    public String setNextQuestion(){
+        List<String> keys = new ArrayList<>(questions.keySet());
+        Random r = new Random();
+        String randomQuestion = keys.get(r.nextInt(keys.size()));
 
+        return randomQuestion;
+
+
+
+    }
 
 
 
